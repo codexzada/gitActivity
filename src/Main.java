93e82fd8c4225796cb96d766676a7f897d066f47
Henzo München Maurer <heinz.crossfire@gmail.com>
2023-01-17 12:46:13 -0300

    Testes

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     static Scanner e = new Scanner(System.in);
     public static void main(String[] args) {
         Pessoa p = new Pessoa();
-        System.out.println("Selecione uma opção: ");
-        int op = e.nextInt();
+        int op = 0;
         while(op != 3){
             menu();
+            System.out.println("Selecione uma opção: ");
+            op = e.nextInt();
+
             switch(op){
                 case 1:
                     addPessoa(p);
                     break;
+                case 2:
+
+                    break;
+                default:
+                    System.out.println("Digite uma opção válida!");
             }
         }
 
@@ -28,12 +36,15 @@ public class Main {
         System.out.println("Digite o CPF: ");
         p.setCpf(e.nextLine());
     }
+    public static void listPessoa(){
+    }
     public static void menu(){
         System.out.println("Tela Principal");
         System.out.println("----------------");
-        System.out.println("Adicionar Pessoa");
-        System.out.println("Listar Pessoas");
-        System.out.println("Sair");
+        System.out.println("1 - Adicionar Pessoa");
+        System.out.println("2 - Listar Pessoas");
+        System.out.println("3 - Sair");
+        System.out.println("----------------");
     }
 
 }
