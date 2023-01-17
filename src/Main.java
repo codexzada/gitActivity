f328345a62788740c3f00764defd62964c2a6fc3
Henzo München Maurer <heinz.crossfire@gmail.com>
2023-01-17 12:24:48 -0300

    Adicionado melhorias

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,39 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner e = new Scanner(System.in);
     public static void main(String[] args) {
         Pessoa p = new Pessoa();
-        Scanner e = new Scanner(System.in);
+        System.out.println("Selecione uma opção: ");
+        int op = e.nextInt();
+        while(op != 3){
+            menu();
+            switch(op){
+                case 1:
+                    addPessoa(p);
+                    break;
+            }
+        }
+
+    }
+    public static void addPessoa(Pessoa p){
 
         System.out.println("Digite o nome: ");
         p.setNome(e.nextLine());
-        
+
         System.out.println("Digite o email: ");
         p.setEmail(e.nextLine());
 
         System.out.println("Digite o CPF: ");
         p.setCpf(e.nextLine());
-        
-        System.out.println(p);
+    }
+    public static void menu(){
+        System.out.println("Tela Principal");
+        System.out.println("----------------");
+        System.out.println("Adicionar Pessoa");
+        System.out.println("Listar Pessoas");
+        System.out.println("Sair");
     }
 
 }
